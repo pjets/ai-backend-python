bb60185a6213a803cf9c417a19995cbf1ae39c20
KBANKNET\preedee.j <preedee.j@kbtg.tech>
2025-08-27 11:50:37 +0700

    initial proj

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontFamily: 'system-ui, -apple-system, ' + "\"Segoe UI\"" + ', Roboto, Ubuntu, "Helvetica Neue", Arial' }}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
